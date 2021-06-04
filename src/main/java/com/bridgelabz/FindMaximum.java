dd3416a47f71cb0fc8d7c61d5b61171c84acf232
datta1808 <bagaledatta18@gmail.com>
2021-06-04 19:43:08 +0530

    REFACTORED : Modified Code to One Generic Method to Find the Maximum

--- src/main/java/com/bridgelabz/FindMaximum.java
+++ src/main/java/com/bridgelabz/FindMaximum.java
@@ -2,42 +2,12 @@ package com.bridgelabz;
 
 public class FindMaximum {
 
-    public Integer findIntegerMaximum(Integer number1, Integer number2, Integer number3) {
-
-        Integer maximum = number1;
-
-        if(number2.compareTo(maximum) > 0) {
-            maximum = number2;
-        }
-        if (number3.compareTo(maximum) > 0) {
-            maximum = number3;
-        }
-        return maximum;
-    }
-
-    public Float findFloatMaximum(Float number1, Float number2, Float number3) {
-
-        Float maximum = number1;
-
-        if (number2.compareTo(maximum) > 0) {
-            maximum = number2;
-        }
-        if (number3.compareTo(maximum) > 0) {
-            maximum = number3;
-        }
-        return maximum;
-    }
-
-    public String findStringMaximum(String number1,String number2,String number3) {
-
-        String maximum = number1;
-
-        if (number2.compareTo(maximum) > 0) {
-            maximum = number2;
-        }
-        if (number3.compareTo(maximum) > 0) {
-            maximum = number3;
-        }
-        return maximum;
+    public  static <T extends Comparable<T>> T testMax(T x, T y, T z){
+        T max = x;
+        if(y.compareTo(max) > 0)
+            max = y;
+        if(z.compareTo(max) > 0)
+            max = z;
+        return max;
     }
 }
--- src/test/java/com/bridgelabz/FindMaximumTest.java
+++ src/test/java/com/bridgelabz/FindMaximumTest.java
@@ -8,55 +8,55 @@ public class FindMaximumTest {
 
     @Test
     public void givenFirstNumber_AsMaxNumber_ShouldReturn_FirstNumber_AsMaximum() {
-        Integer maximum = findMaximum.findIntegerMaximum(800, 70, 115);
+        Integer maximum = findMaximum.testMax(800, 70, 115);
         Assert.assertEquals((Integer) 800, maximum);
     }
 
     @Test
     public void givenSecondNumber_AsMaXNumber_ShouldReturn_SecondNumber_AsMaximum() {
-        Integer maximum1 = findMaximum.findIntegerMaximum(5, 250, 112);
+        Integer maximum1 = findMaximum.testMax(5, 250, 112);
         Assert.assertEquals((Integer) 250, maximum1);
     }
 
     @Test
     public void givenThirdNumber_AsMaXNumber_ShouldReturnThirdNumber_AsMaximum() {
-        Integer maximum2 = findMaximum.findIntegerMaximum(109, 200, 999);
+        Integer maximum2 = findMaximum.testMax(109, 200, 999);
         Assert.assertEquals((Integer) 999, maximum2);
     }
 
     @Test
     public void givenFirstFloat_AsMaxNumber_ShouldReturn_FirstFloat_AsMaximum() {
-        Float max1 = findMaximum.findFloatMaximum(50f, 20f, 10f);
+        Float max1 = findMaximum.testMax(50f, 20f, 10f);
         Assert.assertEquals((Float) 50f, max1);
     }
 
     @Test
     public void givenSecondFloat_AsMaxNumber_ShouldReturn_SecondFloat_AsMaximum() {
-        Float max2 = findMaximum.findFloatMaximum(10f, 30f, 20f);
+        Float max2 = findMaximum.testMax(10f, 30f, 20f);
         Assert.assertEquals((Float) 30f, max2);
     }
 
     @Test
     public void givenThirdFloat_AsMaxNumber_ShouldReturn_ThirdFloat_AsMaximum() {
-        Float max3 = findMaximum.findFloatMaximum(7f, 23f, 40f);
+        Float max3 = findMaximum.testMax(7f, 23f, 40f);
         Assert.assertEquals((Float) 40f, max3);
     }
 
     @Test
     public void givenFirstLetterAsMaximum_ShouldReturnFirstLetterAsMaximum() {
-        String max2 = findMaximum.findStringMaximum("peach", "apple", "banana");
+        String max2 = findMaximum.testMax("peach", "apple", "banana");
         Assert.assertEquals("peach", max2);
     }
 
     @Test
     public void givenSecondLetterAsMaximum_ShouldReturnSecondLetterAsMaximum() {
-        String max2 = findMaximum.findStringMaximum("apple", "peach", "banana");
+        String max2 = findMaximum.testMax("apple", "peach", "banana");
         Assert.assertEquals("peach", max2);
     }
 
     @Test
     public void givenThirdLetterAsMaximum_ShouldReturnThirdLetterAsMaximum() {
-        String max2 = findMaximum.findStringMaximum("apple", "banana", "peach");
+        String max2 = findMaximum.testMax("apple", "banana", "peach");
         Assert.assertEquals("peach", max2);
     }
 }
