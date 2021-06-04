32581c6cf7bb05f811766456cd6d217f485c2802
datta1808 <bagaledatta18@gmail.com>
2021-06-04 16:05:47 +0530

    ADDED : Functionality to Check Max Number at Second Position

--- src/test/java/com/bridgelabz/FindMaximumTest.java
+++ src/test/java/com/bridgelabz/FindMaximumTest.java
@@ -7,9 +7,15 @@ public class FindMaximumTest {
     FindMaximum findMaximum = new FindMaximum();
 
     @Test
-    public void givenFirstNumberAsMaxNumber_ShouldReturn_FirstNumberAsMax() {
-
+    public void givenFirstNumber_AsMaxNumber_ShouldReturn_FirstNumberAsMax() {
         Integer maximum = findMaximum.findIntegerMaximum(800, 70, 115);
         Assert.assertEquals((Integer) 800, maximum);
     }
+
+    @Test
+    public void givenSecondNumber_AsMaXNumber_ShouldReturnSecondNumber_AsMaximum() {
+        Integer maximum1 = findMaximum.findIntegerMaximum(5, 250, 112);
+        Assert.assertEquals((Integer) 250, maximum1);
+    }
+
 }
