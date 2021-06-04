b6741639116df5a981b654043b57e3599026415b
datta1808 <bagaledatta18@gmail.com>
2021-06-04 16:19:46 +0530

    ADDED: Test Case to Check Max Number At Third Position

--- src/test/java/com/bridgelabz/FindMaximumTest.java
+++ src/test/java/com/bridgelabz/FindMaximumTest.java
@@ -7,15 +7,20 @@ public class FindMaximumTest {
     FindMaximum findMaximum = new FindMaximum();
 
     @Test
-    public void givenFirstNumber_AsMaxNumber_ShouldReturn_FirstNumberAsMax() {
+    public void givenFirstNumber_AsMaxNumber_ShouldReturn_FirstNumber_AsMaximum() {
         Integer maximum = findMaximum.findIntegerMaximum(800, 70, 115);
         Assert.assertEquals((Integer) 800, maximum);
     }
 
     @Test
-    public void givenSecondNumber_AsMaXNumber_ShouldReturnSecondNumber_AsMaximum() {
+    public void givenSecondNumber_AsMaXNumber_ShouldReturn_SecondNumber_AsMaximum() {
         Integer maximum1 = findMaximum.findIntegerMaximum(5, 250, 112);
         Assert.assertEquals((Integer) 250, maximum1);
     }
 
+    @Test
+    public void givenThirdNumber_AsMaXNumber_ShouldReturnThirdNumber_AsMaximum() {
+        Integer maximum2 = findMaximum.findIntegerMaximum(109, 200, 999);
+        Assert.assertEquals((Integer) 999, maximum2);
+    }
 }
