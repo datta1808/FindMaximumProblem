3af32532220d5d841c963d968584006a8905126a
datta1808 <bagaledatta18@gmail.com>
2021-06-04 15:40:00 +0530

    ADDED : Functionality to Find the Maximum of 3 Integers

--- src/main/java/com/bridgelabz/FindMaximum.java
+++ src/main/java/com/bridgelabz/FindMaximum.java
@@ -2,6 +2,20 @@ package com.bridgelabz;
 
 public class FindMaximum {
     public static void main(String[] args) {
-        System.out.println("Welcome to Find Maximum Problem using Generics");
+        printMax(findIntegerMaximum(100, 200, 400));
+    }
+    public static Integer findIntegerMaximum(Integer number1, Integer number2, Integer number3) {
+        Integer maximum = number1;
+        if(number2.compareTo(maximum) > 0) {
+            maximum = number2;
+        }
+        if (number3.compareTo(maximum) > 0) {
+            maximum = number3;
+        }
+        return maximum;
+    }
+
+    private static void printMax(Integer max) {
+        System.out.println(" Maximum number is : " + max);
     }
 }
